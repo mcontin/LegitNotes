5984f0d374eb176f05db6085eae897d8e7320d4e
michelebravo <tsam005@tecnicosuperiorekennedy.it>
2016-05-14 18:17:02 +0200

    parallax nel dettaglio (forse un po' inutile ma aspettiamo a toglierlo) + fab per modificare la nota dal dettaglio

--- app/src/main/java/com/legitdevs/legitnotes/DetailNotes.java
+++ app/src/main/java/com/legitdevs/legitnotes/DetailNotes.java
@@ -1,21 +1,58 @@
 package com.legitdevs.legitnotes;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.nineoldandroids.view.ViewHelper;
 
-public class DetailNotes extends AppCompatActivity {
+public class DetailNotes extends AppCompatActivity implements ObservableScrollViewCallbacks {
 
-    Toolbar toolbar;
+    private ObservableScrollView scrollView;
+    private ImageView attached;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_notes);
-        
+
+        scrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        scrollView.setScrollViewCallbacks(this);
+
+        attached = (ImageView) findViewById(R.id.mediaView);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO link per modificare la nota
+            }
+        });
+
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        ViewHelper.setTranslationY(attached, scrollY / 4 * 3);
+
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
 
     }
 }
