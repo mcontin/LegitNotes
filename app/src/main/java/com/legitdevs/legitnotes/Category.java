c657fd42a73833c56e402cdfab4e53fed491c566
Mattia Contin <m.contin.dev@gmail.com>
2016-05-17 00:39:42 +0200

    DISINSTALLATE L'APP -iniziati metodi per gestione delle categorie nel database -cambiato funzionamento dell'id delle note, ora è un UUID (GUID, identificatore unico, prima era un hash calcolato in base a titolo, testo e data, fallato perchè cambiava quando l'utente modificava la nota lol)

--- app/src/main/java/com/legitdevs/legitnotes/Category.java
+++ app/src/main/java/com/legitdevs/legitnotes/Category.java
@@ -2,12 +2,18 @@ package com.legitdevs.legitnotes;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.nfc.tech.NfcA;
 import android.support.v4.content.ContextCompat;
+
+import java.util.HashMap;
 
 /**
  * Created by mattia on 16/05/16.
  */
 public class Category {
+
+    private static final String KEY_NAME = "name";
+    private static final String KEY_COLOR = "color";
 
     private String name;
     private int color;
@@ -28,5 +34,21 @@ public class Category {
         this.name = name;
         this.color = color;
     }
+    public Category(HashMap<String, Object> categories) {
+        name = (String) categories.get(KEY_NAME);
+        color = (Integer) categories.get(KEY_COLOR);
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public HashMap<String, Object> toHashMap(){
+        HashMap<String, Object> hashMap = new HashMap<>();
+
+        hashMap.put(KEY_NAME, name);
+        hashMap.put(KEY_NAME, color);
+
+        return hashMap;
+    }
 }
