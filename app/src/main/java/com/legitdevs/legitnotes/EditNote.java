f32c4dccb6bc08335066c8e179a0789fb3ee1f7b
gabrySuerz <tsam028@tecnicosuperiorekennedy.it>
2016-05-17 13:45:31 +0200

    Rich Text EditNote

--- app/src/main/java/com/legitdevs/legitnotes/EditNote.java
+++ app/src/main/java/com/legitdevs/legitnotes/EditNote.java
@@ -1,6 +1,7 @@
 package com.legitdevs.legitnotes;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +14,12 @@ import com.legitdevs.legitnotes.database.DatabaseManager;
 import java.text.DateFormat;
 import java.util.Date;
 
+import jp.wasabeef.richeditor.RichEditor;
+
 public class EditNote extends AppCompatActivity {
 
     private EditText title;
-    private EditText text;
+    private RichEditor text;
     private Note note;
     private TextView date;
     private Date current;
@@ -30,7 +33,7 @@ public class EditNote extends AppCompatActivity {
         setContentView(R.layout.activity_edit_note);
 
         title = (EditText) findViewById(R.id.editTitle);
-        text = (EditText) findViewById(R.id.editText);
+        text=(RichEditor)findViewById(R.id.editText);
         date = (TextView) findViewById(R.id.creationDate);
         database = new DatabaseManager(this);
 
@@ -40,7 +43,7 @@ public class EditNote extends AppCompatActivity {
         if (receivedBundle != null) {
             note = receivedBundle.getParcelable(NoteDetailActivity.KEY_NOTE);
             title.setText(note.getTitle());
-            text.setText(note.getText());
+            //text.setText(note.getText());
             date.setText(DateFormat.getDateTimeInstance().format(note.getDate()));
             media=note.getMedia();
         } else {
@@ -54,7 +57,7 @@ public class EditNote extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 note.setTitle(title.getText().toString());
-                note.setText(text.getText().toString());
+                //note.setText(text.getText().toString());
                 /*if (note.getId() != 0) {
                     database.updateNote();
                 } else {
@@ -66,8 +69,164 @@ public class EditNote extends AppCompatActivity {
         add_media.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                media+="";
+                media += "";
                 note.setMedia(media);
+            }
+        });
+
+        findViewById(R.id.action_undo).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.undo();
+            }
+        });
+
+        findViewById(R.id.action_redo).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.redo();
+            }
+        });
+
+        findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setBold();
+            }
+        });
+
+        findViewById(R.id.action_italic).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setItalic();
+            }
+        });
+
+        findViewById(R.id.action_subscript).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setSubscript();
+            }
+        });
+
+        findViewById(R.id.action_superscript).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setSuperscript();
+            }
+        });
+
+        findViewById(R.id.action_strikethrough).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setStrikeThrough();
+            }
+        });
+
+        findViewById(R.id.action_underline).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setUnderline();
+            }
+        });
+
+        findViewById(R.id.action_heading1).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setHeading(1);
+            }
+        });
+
+        findViewById(R.id.action_heading2).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setHeading(2);
+            }
+        });
+
+        findViewById(R.id.action_heading3).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setHeading(3);
+            }
+        });
+
+        findViewById(R.id.action_heading4).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setHeading(4);
+            }
+        });
+
+        findViewById(R.id.action_heading5).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setHeading(5);
+            }
+        });
+
+        findViewById(R.id.action_heading6).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setHeading(6);
+            }
+        });
+
+        findViewById(R.id.action_txt_color).setOnClickListener(new View.OnClickListener() {
+            private boolean isChanged;
+
+            @Override public void onClick(View v) {
+                text.setTextColor(isChanged ? Color.BLACK : Color.RED);
+                isChanged = !isChanged;
+            }
+        });
+
+        findViewById(R.id.action_bg_color).setOnClickListener(new View.OnClickListener() {
+            private boolean isChanged;
+
+            @Override public void onClick(View v) {
+                text.setTextBackgroundColor(isChanged ? Color.TRANSPARENT : Color.YELLOW);
+                isChanged = !isChanged;
+            }
+        });
+
+        findViewById(R.id.action_indent).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setIndent();
+            }
+        });
+
+        findViewById(R.id.action_outdent).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setOutdent();
+            }
+        });
+
+        findViewById(R.id.action_align_left).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setAlignLeft();
+            }
+        });
+
+        findViewById(R.id.action_align_center).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setAlignCenter();
+            }
+        });
+
+        findViewById(R.id.action_align_right).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setAlignRight();
+            }
+        });
+
+        findViewById(R.id.action_blockquote).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.setBlockquote();
+            }
+        });
+
+        findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.insertImage("http://www.1honeywan.com/dachshund/image/7.21/7.21_3_thumb.JPG",
+                        "dachshund");
+            }
+        });
+
+        findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.insertLink("https://github.com/wasabeef", "wasabeef");
+            }
+        });
+        findViewById(R.id.action_insert_checkbox).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                text.insertTodo();
             }
         });
     }
